0b7410ea3e7ca31b2c8e1b32cffa42c83707cb6a
Fabiano Pereira <fabianop971@gmail.com>
2024-09-25 15:13:42 -0300

    Feat: add website in pwa

--- src/app/(authenticated)/dash/layout.tsx
+++ src/app/(authenticated)/dash/layout.tsx
@@ -1,37 +1,64 @@
-import LayoutDash from "@/components/layoutDash";
-import { VisionContextProvider } from "@/context";
-import "@/styles/globals.scss";
-import { config } from "@/styles/toast";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { Toaster } from "react-hot-toast";
-import styled from "./style.module.scss";
+import LayoutDash from "@/components/layoutDash"
+import { VisionContextProvider } from "@/context"
+import "@/styles/globals.scss"
+import { config } from "@/styles/toast"
+import type { Metadata, Viewport } from "next"
+import { Inter } from "next/font/google"
+import { Toaster } from "react-hot-toast"
+import styled from "./style.module.scss"
 
-const inter = Inter({ subsets: ["latin"], weight: ["300", "400", "500", "600", "700", "900"] });
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "900"]
+})
 
 export const metadata: Metadata = {
-  title: "Pagina Inicial | Vision Teck",
-  description: "Vision Teck investindo em um mundo melhor",
-  icons: {
-    icon: '/assets/favicon.png'
-  }
-};
+  title: "Vision Teck",
+  description: "Dashboard | Vision Teck",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: [
+    "nextjs",
+    "next14",
+    "pwa",
+    "next-pwa",
+    "tecnologia",
+    "investimentos"
+  ],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    {
+      name: "Fabiano Pereira",
+      url: "https://portfolio-fabianopereiraone.vercel.app/"
+    }
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "/assets/favicon.png" },
+    { rel: "icon", url: "/assets/favicon.png" }
+  ]
+}
+
+export const viewport: Viewport = {
+  themeColor: "#0D0D0D"
+}
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang='pt-BR'>
       <body className={`${inter.className} ${styled.contentLayout}`}>
         <VisionContextProvider>
           <LayoutDash>
             {children}
-            <Toaster position="top-right" toastOptions={config} />
+            <Toaster position='top-right' toastOptions={config} />
           </LayoutDash>
         </VisionContextProvider>
       </body>
     </html>
-  );
+  )
 }
