42b9543e5cdf65fd3a676c0769792e1c230de0cb
Fabiano Pereira <fabianop971@gmail.com>
2024-10-04 11:49:49 -0300

    Feat: create component video

--- src/app/(authenticated)/dash/trains/[slug]/parts/bodyClass/parts/videoComponent/index.tsx
+++ src/app/(authenticated)/dash/trains/[slug]/parts/bodyClass/parts/videoComponent/index.tsx
@@ -1,5 +1,30 @@
+import { Class } from "@/types/class"
+import ReactPlayer from "react-player"
 import styled from "./style.module.scss"
 
-export const VideoComponent = () => {
-  return <div className={styled.container}>Video</div>
+export const VideoComponent = ({ classActive }: { classActive: Class }) => {
+  if (!classActive) return
+
+  const preventContextMenu = (e: any) => {
+    e.preventDefault()
+  }
+
+  return (
+    <div className={styled.container} onContextMenu={preventContextMenu}>
+      <ReactPlayer
+        url={classActive?.linkClass}
+        width='100%'
+        height='100%'
+        controls={true}
+        config={{
+          file: {
+            attributes: {
+              controlsList: "nodownload"
+            }
+          }
+        }}
+        playing={false}
+      />
+    </div>
+  )
 }
